c85ae711eca4931b441bd52425d6f3233e40cd47
EwaWisimito <awemaj@gmail.com>
2019-03-23 10:46:16 +0100

    redux

---
+++ src/store/counter.js
@@ -0,0 +1,25 @@
+
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+const INCREMENT_COUNTER =  'INCREMENT_COUNTER'
+
+
+
+export const inc = () => ({
+    type: INCREMENT_COUNTER
+})
+export const dec = () => ({
+    type: DECREMENT_COUNTER
+})
+
+
+
+export const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case INCREMENT_COUNTER:
+    return state +1
+    case DECREMENT_COUNTER:
+    return state -1
+    default:
+    return state
+  }
+}
